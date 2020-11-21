22d4829e52233ecd8e79116f31e22262db355c64
Jason Lothamer <jhlothamer@gmail.com>
2020-11-21 16:46:53 -0500

    add resources to repair or get from reclaim on tile popup menu

--- game/scenes/ui/tile_popup_menu.gd
+++ game/scenes/ui/tile_popup_menu.gd
@@ -36,10 +36,24 @@ func show_for_structure(structure: StructureMgr.StructureData, position: Vector2
 	else:
 		set_item_text(DISABLE_ITEM_ID, "Disable")
 	_current_structure = structure
-	popup(Rect2(position, rect_size))
-	#rect_position = position
-	#show()
 
+	#var x = structure.structure_type_id
+	var structure_mgr := StructureMgr.get_structure_mgr()
+	var structure_metadata: StructureMgr.StructureMetadata = structure_mgr.get_structure_metadata(structure.structure_type_id)
+	var repair_cost_string = _make_resources_string(structure_metadata.get_repair_resources(), "-")
+	set_item_text(REPAIR_ITEM_ID, "Repair: " + repair_cost_string)
+	var reclaim_cost_string = _make_resources_string(structure_metadata.get_reclamation_resources(), "+")
+	set_item_text(RECLAIM_ITEM_ID, "Reclaim: " + reclaim_cost_string)
+
+	popup(Rect2(position, rect_size))
+
+func _make_resources_string(resources: Dictionary, amount_prefix: String) -> String:
+	var s = ""
+	for resource_name in resources.keys():
+		if s.length() > 0:
+			s += ", "
+		s += amount_prefix + str(int(resources[resource_name])) + " " + resource_name
+	return s
 
 func _on_TilePopupMenu_id_pressed(id):
 	if id == REPAIR_ITEM_ID:
