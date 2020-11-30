6f979d2276f62a5a4268498e5cf663002bef4881
Jason Lothamer <jhlothamer@gmail.com>
2020-11-30 06:54:30 -0500

    move pop crash loose dialog to it's own scene

--- game/addons/godot_helper_pack/core/audio/scene_sound_track.gd
+++ game/addons/godot_helper_pack/core/audio/scene_sound_track.gd
@@ -9,13 +9,13 @@ var _scene_names := []
 func _ready():
 	for scene_file_path in scenes:
 		var scene_name = StringUtil.get_file_name(scene_file_path)
-		_scene_names.append(scene_name)
+		_scene_names.append(scene_name.replace("_", "").to_lower())
 	
 	if startup and _scene_names.size() > 0:
 		check_scene_and_play(_scene_names[0])
 
-func check_scene_and_play(scene_name) -> bool:
-	if !_scene_names.has(scene_name):
+func check_scene_and_play(scene_name: String) -> bool:
+	if !_scene_names.has(scene_name.to_lower()):
 		return false
 	for c in get_children():
 		if c is AudioStreamPlayer or c is AudioStreamPlayer2D or c is AudioStreamPlayer3D:
