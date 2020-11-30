8f88b73ad201cc9be4e01b10ece3e634176af44b
Jason Lothamer <jhlothamer@gmail.com>
2020-11-30 07:00:10 -0500

    fix issue with menu scene background stars not moving

--- game/scenes/misc/stars_and_nebula_background_non_game.gd
+++ game/scenes/misc/stars_and_nebula_background_non_game.gd
@@ -4,8 +4,12 @@ extends TextureRect
 export var speed := 1.0 setget _set_speed
 
 
+func _ready():
+	_set_speed(speed)
+
 
 func _set_speed(value):
 	speed = value
 	var shader_material: ShaderMaterial = material
 	shader_material.set_shader_param("speed", value)
+
