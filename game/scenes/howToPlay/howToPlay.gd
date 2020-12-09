8f8ed62ee7a0fde421814ef2b4bf1e0f41f47dee
Jason Lothamer <jhlothamer@gmail.com>
2020-12-09 06:45:22 -0500

    add intro vo and sync up

--- game/scenes/howToPlay/howToPlay.gd
+++ game/scenes/howToPlay/howToPlay.gd
@@ -18,10 +18,58 @@ onready var _instructions_container := $InstructionsMarginContainer
 onready var _instructions := $InstructionsMarginContainer/VBoxContainer/Instructions
 onready var _skip_btn := $SkipMarginContainer/SkipBtn
 onready var _stat_mgr := $StatsMgr
+onready var _intro1_vo := $Intro1AudioStreamPlayer
+onready var _intro2_vo := $Intro2AudioStreamPlayer
+onready var _intro3_vo := $Intro3AudioStreamPlayer
 
 var _invisible_color := Color(1.0, 1.0, 1.0, 0.0)
 var _visible_color := Color.white
 var _skipped := false
+
+var _intro_lines_begin1 := [
+	0.00,
+	2.034, 
+	4.018, 
+	6.60,
+	9.512, 
+	12.583,
+	14.327]
+var _intro_lines_end1 := [
+	1.555,
+	3.619,
+	6.521,
+	8.993,
+	11.875,
+	14.337,
+	16.521]
+var _intro_lines_begin2 := [
+	0.00,
+	2.639,
+	5.610,
+	9.583,
+	11.534]
+var _intro_lines_end2 := [
+	2.397,
+	5.354,
+	8.725,
+	11.534,
+	13.469]
+var _intro_lines_begin3 := [
+	0.000,
+	2.279,
+	3.997,
+	5.59, 
+	8.76, 
+	9.935,
+	11.288]
+var _intro_lines_end3 := [
+	2.074,
+	3.757,
+	5.599,
+	8.128,
+	9.935,
+	11.199,
+	14.858]
 
 func _ready():
 	_intro_lines_1.visible = false
@@ -38,7 +86,7 @@ func _start_intro():
 	_timer.start(time_between_line_fade_ins_seconds)
 	yield(_timer,"timeout")
 
-	_intro_labels(_intro_lines_1)
+	_intro_labels(_intro_lines_1, _intro1_vo, _intro_lines_begin1, _intro_lines_end1)
 	yield(self, "intro_lines_complete")
 	_tween.interpolate_property(_intro_lines_1, "modulate", _intro_lines_1.modulate, _invisible_color, time_between_line_fade_ins_seconds,Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	_tween.start()
@@ -47,7 +95,7 @@ func _start_intro():
 	_timer.start(time_between_line_fade_ins_seconds)
 	yield(_timer,"timeout")
 
-	_intro_labels(_intro_lines_2)
+	_intro_labels(_intro_lines_2, _intro2_vo, _intro_lines_begin2, _intro_lines_end2)
 	yield(self, "intro_lines_complete")
 	_tween.interpolate_property(_intro_lines_2, "modulate", _intro_lines_2.modulate, _invisible_color, time_between_line_fade_ins_seconds,Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	_tween.start()
@@ -56,7 +104,7 @@ func _start_intro():
 	_timer.start(time_between_line_fade_ins_seconds)
 	yield(_timer,"timeout")
 
-	_intro_labels(_intro_lines_3)
+	_intro_labels(_intro_lines_3, _intro3_vo, _intro_lines_begin3, _intro_lines_end3)
 	yield(self, "intro_lines_complete")
 	_tween.interpolate_property(_intro_lines_3, "modulate", _intro_lines_3.modulate, _invisible_color, time_between_line_fade_ins_seconds,Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	_tween.start()
@@ -67,18 +115,32 @@ func _start_intro():
 	
 	_on_SkipBtn_pressed()
 
-func _intro_labels(parent: Control):
+func _intro_labels(parent: Control, intro_vo: AudioStreamPlayer, intro_begin: Array, intro_end: Array) -> void:
 	var labels = parent.get_children()
 	for label in labels:
 		label.modulate = _invisible_color
 	parent.visible = true
 	
-	for label in labels:
-		_tween.interpolate_property(label, "modulate", label.modulate, _visible_color, line_fade_in_time_seconds,Tween.TRANS_LINEAR,Tween.EASE_IN)
+	intro_vo.play()
+	
+	#for label in labels:
+	for i in range(labels.size()):
+		if i > 0:
+			var begin_pause_time = intro_begin[i] - intro_end[i-1]
+			if begin_pause_time > 0:
+				_timer.start(begin_pause_time)
+				yield(_timer,"timeout")
+		
+		var current_fade_in_time = intro_end[i] - intro_begin[i]
+		var label = labels[i]
+		_tween.interpolate_property(label, "modulate", label.modulate, _visible_color, current_fade_in_time,Tween.TRANS_LINEAR,Tween.EASE_IN)
 		_tween.start()
 		yield(_tween, "tween_all_completed")
-		_timer.start(time_between_line_fade_ins_seconds)
-		yield(_timer,"timeout")
+#		_timer.start(time_between_line_fade_ins_seconds)
+#		yield(_timer,"timeout")
+	
+	if intro_vo.playing:
+		yield(intro_vo,"finished")
 	
 	emit_signal("intro_lines_complete")
 
