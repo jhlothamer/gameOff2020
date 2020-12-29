1d4bf8f1a1f75807002961af4b554187ea572e24
Jason Lothamer <jhlothamer@gmail.com>
2020-12-28 20:01:54 -0500

    add info to the drawer area, make buttons disabled with insuff. resources

--- game/scenes/ui/construction_drawer_button_group.gd
+++ game/scenes/ui/construction_drawer_button_group.gd
@@ -6,13 +6,14 @@ signal construction_button_mouse_exited(structure_type)
 
 export (StructureMgr.StructureTileType) var structure_type: int
 
-var disabled := false setget _set_disabled
-var collapsed := true setget _set_collapsed
 var shortcut: ShortCut
 
 onready var _texture_button := $Panel/MarginContainer/TextureButton
 onready var _short_name_label := $ShortNameLabel
 onready var _cost_label := $CostLabel
+onready var _functional_label := $FunctionalLabel
+onready var _population_supported_label := $PopulationSupportedLabel
+onready var _population_possible_label := $PopulationPossibleLabel
 onready var _mouse_over_sound := $MouseOverStreamPlayer
 onready var _shortcut_label := $Panel/MarginContainer/TextureButton/ShortcutLabel
 onready var _highlight_texture := $Panel/MarginContainer/TextureButton/HighlightTextureRect
@@ -47,6 +48,8 @@ func _ready():
 	SignalMgr.register_publisher(self, "construction_tool_bar_clicked")
 	SignalMgr.register_publisher(self, "construction_button_mouse_entered")
 	SignalMgr.register_publisher(self, "construction_button_mouse_exited")
+	SignalMgr.register_subscriber(self, "resources_updated", "_on_resources_updated")
+	SignalMgr.register_subscriber(self, "structure_state_changed", "_on_structure_state_changed")
 	
 	_texture_button.texture_normal = load(_normal_icons[structure_type])
 	_texture_button.texture_disabled = load(_disabled_icons[structure_type])
@@ -76,6 +79,7 @@ func _init_labels():
 	_cost_label.text = construction_cost_string
 	_short_name_label.text = structure_metadata.get_short_name()
 	#_texture_button.hint_tooltip = structure_metadata.get_name()
+	_update_labels()
 
 
 func _make_resources_string(resources: Dictionary, amount_prefix: String) -> String:
@@ -85,16 +89,6 @@ func _make_resources_string(resources: Dictionary, amount_prefix: String) -> Str
 			s += ", "
 		s += amount_prefix + str(int(resources[resource_name])) + " " + resource_name
 	return s
-
-func _set_disabled(value):
-	disabled = value
-	if _texture_button != null:
-		_texture_button.disabled = value
-
-
-func _set_collapsed(value):
-	collapsed = value
-	visible = !collapsed
 
 
 func _on_TextureButton_mouse_entered():
@@ -118,3 +112,37 @@ func _on_TextureButton_pressed():
 func _on_TextureButton_mouse_exited():
 	_highlight_texture.visible = false
 	emit_signal("construction_button_mouse_exited", structure_type)
+
+func _on_resources_updated():
+	var resource_mgr: ResourceMgr = ServiceMgr.get_service(ResourceMgr)
+	if resource_mgr == null:
+		return
+	_texture_button.disabled = !resource_mgr.have_enough_resources_for_constructions(structure_type)
+
+func _update_labels() -> void:
+	var structure_mgr: StructureMgr = ServiceMgr.get_service(StructureMgr)
+	var structure_metadata := structure_mgr.get_structure_metadata(structure_type)
+	var structure_name = structure_metadata.get_name()
+	var short_cut_key = structure_metadata.get_construction_short_cut_key()
+	var structures = structure_mgr.get_structures_by_type_id(structure_type)
+	var total = structures.size()
+	var functioning_structures = structure_mgr.get_functioning_structures_by_type_id(structure_type)
+	var functioning = functioning_structures.size()
+	_functional_label.text = "%d/%d" % [functioning, total]
+	
+	var stats_mgr: StatsMgr = ServiceMgr.get_service(StatsMgr)
+	var stat := stats_mgr.get_stat_provided_by_structure_type_id(structure_type)
+	if stat != null && stat.has_population_schedule():
+		var units_per_structure := stat.get_units_per_structure()
+		var total_population_provided = functioning * units_per_structure
+		var population_currently_supported = stats_mgr.calc_structure_produced_stats(stat, functioning_structures)
+		#_population_label.text =  "%d/%d" % [population_currently_supported, total_population_provided]
+		_population_supported_label.text = "%d" % population_currently_supported
+		_population_possible_label.text = "%d" % total_population_provided
+	
+	
+
+
+func _on_structure_state_changed(tile_map_cell: Vector2) -> void:
+	_update_labels()
+
